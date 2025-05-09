50d901c25950e29a04d428fecbc0efa4f2281d7d
Ganesh Kumar R <ganesh.ramani@alifsemi.com>
2025-05-09 12:37:40 +0530

    west: flash: integrated with SE Tools
    Integrated app-gen-toc and app-write-mram binary to write
    zephyr build binary into MRAM with ToC.
    
    Signed-off-by: Ganesh Kumar R <ganesh.ramani@alifsemi.com>

--- scripts/west_commands/runners/alif_flash.py
+++ scripts/west_commands/runners/alif_flash.py
@@ -4,19 +4,25 @@
 Runner for Alif binary image burner.
 """
 
-import argparse
-import os
 import sys
-import re
-from pathlib import Path
 import json
 
+from pathlib import Path
 from runners.core import ZephyrBinaryRunner, RunnerCaps
 
 class AlifImageBinaryRunner(ZephyrBinaryRunner):
     '''Runner front-end for Alif Image Flasher.'''
 
+    #Address and Location properties.
     home_path = str(Path.home())
+    zephyr_repo = str(Path.cwd())
+    hp_itcm_load_address = '0x50000000'
+    he_itcm_load_address = '0x58000000'
+    he_mram_boot_address = '0x80000000'
+    hp_mram_boot_address = '0x80200000'
+    cfg_ip_file = '/Alif_Tools/build/config/app-cpu-stubs.json'
+    cfg_op_file = '/Alif_Tools/build/config/tmp-app-cpu.json'
+    exe_dir = home_path + '/Alif_Tools'
 
     def __init__(self, cfg, toc_create='app-gen-toc', toc_write='app-write-mram',
                  erase=False, reset=True):
@@ -50,36 +56,50 @@ class AlifImageBinaryRunner(ZephyrBinaryRunner):
             self.logger.info(f'Windows not yet Supported')
             return
         
-        self.require(self.gen_toc)
+        #check tools availability
+        self.require(self.gen_toc, self.exe_dir)
 
-        kwargs['cwd'] ='/home/ganesh/Alif_Tools/'
+        #set Execution directory
+        kwargs['cwd'] = self.exe_dir
 
         fls_addr = self.flash_address_from_build_conf(self.build_conf)
-        self.logger.info(f'binary address 0x{fls_addr:x}')
+        fls_addr = f'0x{fls_addr:x}'
 
-        if self.build_conf.getboolean('CONFIG_SOC_E7_DK_RTSS_HP'):
-            self.logger.info("It is HighPerformance Core")
+        self.logger.info(f'binary address {fls_addr}')
+
+        if self.build_conf.getboolean('CONFIG_RTSS_HP'):
+            self.logger.info("..build for HighPerformance Core")
             build_core = "hp"
         else:   
-            self.logger.info("It is HighEfficency Core")
+            self.logger.info("..build for HighEfficency Core")
             build_core = "he"
 
-        self.prepare_json(build_core, fls_addr)
+        #copy zephyr binary
+        self.check_call( ['cp',
+                          self.zephyr_repo + '/build/zephyr/zephyr.bin',
+                          self.exe_dir + '/build/images/'])
 
-        self.check_call([self.gen_toc, '-f',
-                    self.home_path +
-                    '/Alif_Tools/build/config/app_cpu_stubs.json'],
-                    **kwargs)
+        #Prepare json to create AToC.
+        self.prepare_json(self.logger, build_core, fls_addr)
 
-        #Write ToC.
+        #Generte ToC
+        self.check_call(['./' + self.gen_toc, '-f',
+                        self.home_path +
+                        self.cfg_op_file],
+                        **kwargs)
+
+        #Write ToC with sudo access.
+        self.check_call(['sudo', './' + self.write_toc, '-p'], **kwargs)
 
     @classmethod
-    def prepare_json(cls, build_core, fls_addr):
-        
-        with open(cls.home_path +'/Alif_Tools/build/config/app-cpu-stubs.json', 'r') as conf_file:
-            json_data = json.load(conf_file)
+    def prepare_json(cls, logger, build_core, fls_addr):
+        try:
+            with open(cls.home_path + cls.cfg_ip_file, 'r') as conf_file:
+                json_data = json.load(conf_file)
+        except IOError as err:
+            logger.error(f"Can't open file to read {cls.home_path + cls.cfg_ip_file}")
 
-        if build_core is "hp" :
+        if build_core == "hp" :
             cpu_node = json_data["HP_APP"]
         else :
             cpu_node = json_data["HE_APP"]
@@ -88,18 +108,23 @@ class AlifImageBinaryRunner(ZephyrBinaryRunner):
         cpu_node["binary"] = "zephyr.bin"
 
         #verify flash address
-        if fls_addr == "0x0" and cpu_node.get('mramAdress') is not None:
-            del cpu_node['mramAddress']
-            if build_core is "hp":
-                cpu_node["loadAddress"] = 0x50000000
+        if fls_addr == '0x0':
+            if build_core == "hp":
+                cpu_node["loadAddress"] = cls.hp_itcm_load_address
             else :
-                cpu_node['loadAddress'] = 0x58000000   
+                cpu_node['loadAddress'] = cls.he_itcm_load_address
             cpu_node["flags"] = ["load","boot"]
 
-        elif fls_addr != 0 and cpu_node.get('loadAddress') is not None:
+        elif fls_addr == cls.he_mram_boot_address or fls_addr == cls.hp_mram_boot_address:
             del cpu_node['loadAddress']
-            cpu_node['mramAddress'] = hex(fls_addr)
+            cpu_node['mramAddress'] = fls_addr
             cpu_node['flags'] = ["boot"]
 
-        with open("test.json", 'w') as file:
-            json.dump(json_data, file, indent=4)
+        else:
+            raise NotImplementedError('Invalid Address')
+
+        try:
+            with open(cls.home_path + cls.cfg_op_file, 'w') as file:
+                json.dump(json_data, file, indent=4)
+        except IOError as err:
+            logger.error(f"Can't open file to write {cls.home_path + cls.cfg_op_file}")
