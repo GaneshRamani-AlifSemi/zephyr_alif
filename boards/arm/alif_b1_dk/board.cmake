699f1ccccc630b641d7a04122f074cdf896d3188
Ganesh Kumar R <ganesh.ramani@alifsemi.com>
2025-08-11 16:28:27 +0530

    boards: arm: alif: west debug jlink support
    adding support for west debug through JLink debugger.
    
    Signed-off-by: Ganesh Kumar R <ganesh.ramani@alifsemi.com>

--- boards/arm/alif_b1_dk/board.cmake
+++ boards/arm/alif_b1_dk/board.cmake
@@ -1,4 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-include(${ZEPHYR_BASE}/boards/common/alif_flash.board.cmake)
+board_runner_args(jlink "--device=AB1C1F4M51820" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/alif_flash.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
--- boards/arm/alif_e1c_dk/board.cmake
+++ boards/arm/alif_e1c_dk/board.cmake
@@ -1,4 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-include(${ZEPHYR_BASE}/boards/common/alif_flash.board.cmake)
 
+board_runner_args(jlink "--device=AE1C1F4051920" "--speed=4000")
+
+include(${ZEPHYR_BASE}/boards/common/alif_flash.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
